0c0e6efd551b03e738adada75ddbe817fe815c80
DmitryStoyan <jombee@yandex.ru>
2024-12-01 19:02:46 +0300

    added actions

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <h2>My Favorite Movies</h2>
     </header>
     <div class="tabs">
-      <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]">Favorite</button>
-      <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]">Search</button>
+      <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]" @click="setTab(1)">Favorite</button>
+      <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]" @click="setTab(2)">Search</button>
     </div>
     <div class="movies" v-if="movieStore.activeTab === 1">
       <div>
@@ -23,6 +23,10 @@
 <script setup>
 import Movie from './components/Movie.vue'
 import { useMovieStore } from './stores/MovieStore';
+
+const setTab = (id) => {
+  movieStore.setActiveTab(id)
+}
 
 const movieStore = useMovieStore();
 </script>
